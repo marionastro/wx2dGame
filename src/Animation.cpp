380dda988a41eb195fd61de354dff34d9b97fb21
Mario Nastro <ijack.cazzate@gmail.com>
2019-12-18 16:18:31 +0100

    Update Animation.cpp

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -1,5 +1,19 @@
 #include "Animation.h"
 
+Animation::Animation(Sprite sprite, int framesCount, Framesheet* framesheet, int* framesIndexes, int delay, bool loop)
+: m_Sprite(sprite)
+, m_FramesCount(framesCount)
+, m_Delay(delay)
+, m_Loop(loop)
+{
+    m_Frames = new Frame[framesCount];
+
+    for (int i = 0; i<framesCount; i++)
+    {
+        m_Frames[i] = framesheet->GetFrame(framesIndexes[i]);
+    }
+}
+
 Animation::Animation(Sprite sprite, int framesCount, Frame* frames, int delay, bool loop)
 : m_Sprite(sprite)
 , m_FramesCount(framesCount)
